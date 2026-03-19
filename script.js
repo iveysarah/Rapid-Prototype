6e8a753d7f53e7fdb39d5187af33d29058e2618a
Sarah Ivey <si102623@ohio.edu>
2026-03-19 11:43:03 -0400

    creating the script.js and index.html page

---
+++ script.js
@@ -0,0 +1,9 @@
+function countWords() {
+  const text = document.getElementById("textInput").value.trim();
+ 
+  const words = text === "" ? 0 : text.split(/\s+/).length;
+  const chars = text.length;
+ 
+  document.getElementById("wordCount").textContent = words;
+  document.getElementById("charCount").textContent = chars;
+}
